4afb62ad92fbb2a981c48813fb74e44b3974f1ee
liwq <liwq@umich.edu>
2023-02-11 19:27:31 -0500

    attempt to construct a single post

--- insta485/js/post.jsx
+++ insta485/js/post.jsx
@@ -1,54 +1,59 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import moment from 'moment';
 
 // The parameter of this function is an object with a string called url inside it.
 // url is a prop for the Post component.
 export default function Post({ url }) {
   /* Display image and post owner of a single post */
+
   console.log(url)
   console.log("debug")
+
   const [imgUrl, setImgUrl] = useState("");
   const [owner, setOwner] = useState("");
-
+  const [ownerImgUrl, setOwnerImg] = useState("");
+  const [timeStamp, setTime] = useState("");
+  const [numLikes, setNumlikes] = useState("");
+  const [comments, setComments] = useState("");
+  const [likeButton, setlikeButton] = useState("");
   useEffect(() => {
     // Declare a boolean flag that we can use to cancel the API request.
     let ignoreStaleRequest = false;
+    //using 2 fetches to construct a list of post info
 
     // Call REST API to get the post's information
-    fetch(url, { credentials: "same-origin" })
-
-      .then((response) => {
-        if (!response.ok) throw Error(response.statusText);
-        return response.json();
-      })
-      .then((data) => {
-        console.log("inner url")
-        const listImg = []
-        // If ignoreStaleRequest was set to true, we want to ignore the results of the
-        // the request. Otherwise, update the state to trigger a new render.
-        if (!ignoreStaleRequest) {
-          for (let i = 0; i < data['results'].length; i++) {
-
-            fetch(data['results'][i]['url'])
-
-              .then((response) => {
-                if (!response.ok) throw Error(response.statusText);
-                return response.json();
-              })
-              .then((data1) => {
-                console.log(data1["imgUrl"])
-                listImg.push(data1["imgUrl"])
-              })
-              .then((listImg) => {
-                console.log(data1["imgUrl"])
-                listImg.push(data1["imgUrl"])
-              })
-          }
-          console.log("end for loop")
-          setImgUrl(...listImg)
+    async function fetchData() {
+      const response = await fetch(url, { credentials: "same-origin" })
+      if (!ignoreStaleRequest) {
+        const data = await response.json();
+        const commentLists = []
+        for (var i = 0, size = data['comments'].length; i < size; i++) {
+          commentLists.push(data['comments'][i]['text'])
         }
-      })
-      .catch((error) => console.log(error));
+        setComments([...commentLists])
+        setImgUrl(data['imgUrl'])
+        setOwner(data['owner'])
+        setOwnerImg(data['ownerImgUrl'])
+        setNumlikes(data['likes']['numLikes'])
+        setTime(moment.utc(data['created']))
+        if (data['likes']['lognameLikesThis']) {
+          setlikeButton("unlike")
+        } else {
+          setlikeButton('like')
+        }
+        var day = moment("")
+        console.log(moment.utc(data['created']))
+        console.log(moment.utc(data['created']).fromNow())
+      }
+
+    }
+    fetchData()
+
+
+
+
+
 
     return () => {
       // This is a cleanup function that runs whenever the Post component
@@ -59,10 +64,16 @@ export default function Post({ url }) {
   }, [url]);
 
   // Render post image and post owner
+
   return (
     <div className="post">
       <img src={imgUrl} alt="post_image" />
       <p>{owner}</p>
+      <p>{timeStamp}</p>
+      <p>{numLikes}</p>
+      <img src={ownerImgUrl} alt="owner_image" />
+      <p>{comments}</p>
+      <button type='"button'>{likeButton}</button>
     </div>
   );
 }
