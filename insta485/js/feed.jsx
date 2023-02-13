2a7b9aa6b5e5600894a279a3db619ec4fb8dcaad
yumin2002 <yumin@umich.edu>
2023-02-13 17:28:10 -0500

    scroll weird behavior

--- insta485/js/feed.jsx
+++ insta485/js/feed.jsx
@@ -1,79 +1,99 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import moment from 'moment';
 import Post from "./post";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 // The parameter of this function is an object with a string called url inside it.
 // url is a prop for the Post component.
-export default function Post() {
-    /* Display image and post owner of a single post */
-    const [imgUrl, setImgUrl] = useState("");
-    const [owner, setOwner] = useState("");
-    const [ownerImgUrl, setOwnerImg] = useState("");
-    const [timeStamp, setTime] = useState("");
-    const [numLikes, setNumlikes] = useState("");
-    const [comments, setComments] = useState("");
-    const [likeButton, setlikeButton] = useState("");
-    useEffect(() => {
-        // Declare a boolean flag that we can use to cancel the API request.
-        let ignoreStaleRequest = false;
-        //using 2 fetches to construct a list of post info
+export default function Feed({ url }) {
+  console.log(getPosts(url));
+  const [postsUrls, setPostsUrls] = useState(getPosts(url));
+  const [hasMore, setHasMore] = useState(true);
 
-        // Call REST API to get the post's information
-        async function fetchData() {
-            const response = await fetch('/api/v1/posts/', { credentials: "same-origin" })
-            if (!ignoreStaleRequest) {
-                const data = await response.json();
-                const commentLists = []
-                for (var i = 0, size = data['comments'].length; i < size; i++) {
-                    commentLists.push(data['comments'][i]['text'])
-                }
-                setComments([...commentLists])
-                setImgUrl(data['imgUrl'])
-                setOwner(data['owner'])
-                setOwnerImg(data['ownerImgUrl'])
-                setNumlikes(data['likes']['numLikes'])
-                setTime(moment.utc(data['created']).fromNow())
-                if (data['likes']['lognameLikesThis']) {
-                    setlikeButton("unlike")
-                } else {
-                    setlikeButton('like')
-                }
+  function getPosts(curr_url) {
+    // Declare a boolean flag that we can use to cancel the API request.
+    let ignoreStaleRequest = false;
+    var initialPosts = [];
+    // Call REST API to get the post's information
+    fetch(curr_url, { credentials: "same-origin" })
+      .then((response) => {
+        if (!response.ok) throw Error(response.statusText);
+        return response.json();
+      })
+      .then((data) => {
+        // If ignoreStaleRequest was set to true, we want to ignore the results of the
+        // the request. Otherwise, update the state to trigger a new render.
+        initialPosts = data["results"].map((curr) => {
+          return curr["url"];
+        });
+      })
+      .catch((error) => console.log(error));
+    console.log(initialPosts);
+    return initialPosts;
+  }
 
-
-            }
-
+  function getMorePost() {
+    var curr_url = url;
+    // Declare a boolean flag that we can use to cancel the API request.
+    let ignoreStaleRequest = false;
+    // Call REST API to get the post's information
+    fetch(curr_url, { credentials: "same-origin" })
+      .then((response) => {
+        if (!response.ok) throw Error(response.statusText);
+        response.json();
+      })
+      .then((data) => {
+        // If ignoreStaleRequest was set to true, we want to ignore the results of the
+        // the request. Otherwise, update the state to trigger a new render.
+        if (!ignoreStaleRequest) {
+          console.log(data["next"]);
+          console.log(date["result"]);
+          if (data["next"] == "") {
+            setHasMore(false);
+          } else {
+            setHasMore(true);
+          }
+          setPostsUrls(
+            data["results"].map((curr) => {
+              return curr["url"];
+            })
+          );
+          console.log(hasMore);
+          console.log(postsUrls);
         }
-        fetchData()
+      })
+      .catch((error) => console.log(error));
 
+    return () => {
+      // This is a cleanup function that runs whenever the Post component
+      // unmounts or re-renders. If a Post is about to unmount or re-render, we
+      // should avoid updating state.
+      ignoreStaleRequest = true;
+    };
+  }
 
-
-
-
-
-        return () => {
-            // This is a cleanup function that runs whenever the Post component
-            // unmounts or re-renders. If a Post is about to unmount or re-render, we
-            // should avoid updating state.
-            ignoreStaleRequest = true;
-        };
-    }, [url]);
-
-    // Render post image and post owner
-
-    return (
-        <div className="post">
-            <img src={imgUrl} alt="post_image" />
-            <p>{owner}</p>
-            <p>{timeStamp}</p>
-            <p>{numLikes}</p>
-            <img src={ownerImgUrl} alt="owner_image" />
-            <p>{comments}</p>
-            <button type='"button'>{likeButton}</button>
-        </div>
-    );
+  // Render post image and post owner
+  return (
+    <InfiniteScroll
+      dataLength={postsUrls.length}
+      next={getMorePost}
+      hasMore={hasMore}
+      loader={<h3> Loading...</h3>}
+      endMessage={<h4>Nothing more to show</h4>}
+    >
+      <div>
+        {postsUrls.map((postUrl) => {
+          return (
+            <div>
+              <Post url={postUrl} />
+            </div>
+          );
+        })}
+      </div>
+    </InfiniteScroll>
+  );
 }
 
 Post.propTypes = {
-    url: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 };
--- insta485/js/main.jsx
+++ insta485/js/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Post from "./post";
+import Feed from "./feed";
 
 // Create a root
 const root = createRoot(document.getElementById("reactEntry"));
 
 // This method is only called once
 // Insert the post component into the DOM
-root.render(<Post url="/api/v1/posts/1/" />);
+root.render(<Feed url="/api/v1/posts/" />);
